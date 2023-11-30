de92a29337333b14a4fff68c00198b478dd7009b
Abhishek <abhishekparmar162@gmail.com>
2023-11-30 14:47:28 +0530

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col xl:flex-row ">
-      <div>
-        <p>
+    <main className="flex items-center flex-col lg:flex-row bg-[#1E1919] dark:bg-slate-800 ">
+      <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 space-y-5 text-white ">
+        <p className="text-5xl font-bold">
           Everything you and your business need to work efficiently, all in one
           place
         </p>
-        <p>
+        <p className="pb-20">
           Collaborate seamlessly and deliver work faster from anywhere with
           Dropbox. Securely store your content, edit PDFs, share videos, sign
           documents and track file engagement—without leaving Dropbox.
         </p>
-        <Link href="/dashboard">
-          <Button>
-            Try it for free{" "}
-            <span>
-              <ArrowRight />
-            </span>
-          </Button>
+        <Link
+          href="/dashboard"
+          className="cursor-pointer flex bg-blue-700 p-5 w-fit hover:bg-blue-400 "
+        >
+          Try it for free <ArrowRight className="ml-10" />
         </Link>
       </div>{" "}
-      <video
-        className="mx-auto w-[32rem] h-full"
-        autoPlay={true}
-        loop={true}
-        src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
-      ></video>
-    </div>
+      <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+        <video className="rounded-lg" autoPlay={true} loop={true} muted>
+          <source src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4" />
+        </video>
+      </div>
+    </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import Logo from "@/assests/Logo.png";
 import { DarkModeToggler } from "./DarkModeToggler";
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between">
-      <div className="flex items-center space-x-2">
-        <div className="bg-blue-500 h-12 w-12">
-          <Image className="invert" src={Logo} alt="Dropbox Logo" />
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-blue-500 w-fit ">
+          <Image
+            className="invert"
+            height={50}
+            width={50}
+            src={Logo}
+            alt="Dropbox Logo"
+          />
         </div>
         <p className="font-bold text-xl"> Dropbox</p>
-      </div>
-      <div className="flex items-center space-x-3 pr-2 ">
+      </Link>
+      <div className="flex items-center space-x-3 px-5 ">
         <DarkModeToggler />
         <UserButton afterSignOutUrl="/" />{" "}
+        <SignedOut>
+          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+        </SignedOut>
       </div>
     </header>
   );
